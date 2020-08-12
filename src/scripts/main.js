bf32c54ce37cd3e9dbb476022a9e7551c92f3708
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-12 16:59:25 +0700

    Refactor removeBook() method

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -62,28 +62,22 @@ function main() {
     };
 
     const removeBook = (bookId) => {
-        // Create the instance of XMLHttpRequest
-        const xhr = new XMLHttpRequest();
-
-        // Set the callback if the response is success and error
-        xhr.onload = function () {
-            const responseJson = JSON.parse(this.responseText);
-            showResponseMessage(responseJson.message);
-            getBook();
-        }
-
-        xhr.onerror = function () {
-            showResponseMessage();
-        }
-
-        // Create DELETE request and set the target URL
-        xhr.open("DELETE", `${baseUrl}/delete/${bookId}`);
-
-        // Set Content-Type and X-Auth-Token properties on Header request
-        xhr.setRequestHeader("X-Auth-Token", "12345");
-
-        // Send the request
-        xhr.send();
+        fetch(`${baseUrl}/delete/${bookId}`, {
+            method: "DELETE",
+            headers: {
+                "X-Auth-Token": "12345"
+            }
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(responseJson => {
+                showResponseMessage(responseJson.message);
+                getBook();
+            })
+            .catch(error => {
+                showResponseMessage(error);
+            })
     };
 
 
